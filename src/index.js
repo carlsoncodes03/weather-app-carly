5276056126011521af4bddfd9866df0e3d5d9225
carlsoncodes03 <142261008+carlsoncodes03@users.noreply.github.com>
2023-10-05 15:51:33 -0600

    copied over from csb
    with geolocation (not working yet) and default location (search now not working)

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+function changeCity(city) {
+  let units = "imperial";
+  let apiKey = "6010503ffcf1560b8aef47f39758b9ba";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(`${apiUrl}`).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  search(city);
+}
+
+function showTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#weather-location").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  //let uvIndexElement = document.querySelector("#uv-index");
+}
+
+function showLocation(position) {
+  console.log(position);
+  navigator.geolocation.getCurrentPosition(showLocation);
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "6010503ffcf1560b8aef47f39758b9ba";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
+  axios.get(`${apiUrl}`).then(showTemperature);
+}
+
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let weekday = days[now.getDay()];
+let time = `${hours}:${minutes}`;
+let dateAndTime = document.querySelector("#date-and-time");
+dateAndTime.innerHTML = `${weekday}, ${time}`;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", changeCity);
+
+document
+  .querySelector("#current-location-btn")
+  .addEventListener("click", showLocation);
+
+//changeCity("New York");
